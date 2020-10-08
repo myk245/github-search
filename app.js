55f2388a0bd538762ec788725f971e9d83fcfe75
myk245 <myk245@nyu.edu>
2020-10-08 14:51:45 -0400

    fetch and display repos for users

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ searchUser.addEventListener('keyup', (event) => {
          .then(data => {
             if (data.profile.message === 'Not Found') {
                // Show alert
-               ui.showAlert('User not found.',  'alert alert-danger');
+               ui.showAlert('User not found.', 'alert alert-danger');
             } else {
                // Show profile
                ui.showProfile(data.profile); 
+               // Show repos
+               ui.showRepos(data.repos); 
             }
       })
    } else {
--- ui.js
+++ ui.js
@@ -28,7 +28,33 @@ class UI {
             </div>
          </div>
          <h3 class="page-heading mb-3">Latest Repos</h3>
+         <div id="repos"></div>
       `;
+   }
+
+   // Show user repos
+   showRepos(repos) {
+      let output = ''; 
+
+      repos.forEach(function (repo) {
+         output += `
+         <div class="card card-body mb-2">
+            <div class="row">
+               <div class="col-md-6">
+                  <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+               </div>
+               <div class="col-md-6">
+                  <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                  <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                  <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+               </div>
+            </div>
+         </div>
+         `
+      }); 
+
+      // Output repos
+      document.getElementById('repos').innerHTML = output; 
    }
 
    // Show alert message
@@ -48,10 +74,10 @@ class UI {
       // Insert alert
       container.insertBefore(div, search); 
 
-      // Timeout after 3 secs
+      // Timeout after 2 secs
       setTimeout(() => {
          this.clearAlert();
-      }, 3000);
+      }, 2000);
    }
 
    // Clear alert message
